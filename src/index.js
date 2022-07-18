13b3807d0ee3865d2e975071f1ddfbeaf3f8b95c
yuriipol <104125040+yuriipol@users.noreply.github.com>
2022-07-18 20:50:19 +0300

    add library

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import Notiflix from 'notiflix';
+import templateCard from './templates/card.hbs';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
