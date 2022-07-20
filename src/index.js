6a8bc81d6b0cb39fe33e6463df7a1735438134fc
yuriipol <104125040+yuriipol@users.noreply.github.com>
2022-07-20 15:18:04 +0300

    add comments

--- src/index.js
+++ src/index.js
@@ -1,35 +1,44 @@
+//Импортируем библиотеки, темплейты и наш класс
 import Notiflix from 'notiflix';
 import templateCard from './templates/card.hbs';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import FetchSearchImages from './js/images-api';
 
+//Получаем ссылки на наши елементы самой формы, галереи и кнопки загрузить еще
 const searchForm = document.querySelector('#search-form');
 const loadMore = document.querySelector('.load-more');
 const gallary = document.querySelector('.gallery');
 
+//Создаем экземпляр класса
 const fetchSearch = new FetchSearchImages();
 
+//Вешаем события прослушивания на форму и на кнопку загрузить еще
 searchForm.addEventListener('submit', onSearchFormSubmit);
 loadMore.addEventListener('click', onClickLoadMore);
 
+//Присваеваем переменной libraryLithBox ссылку на библиотеку SimpleLightbox
 const libraryLithBox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
 
+//Создаем асинхронную функцию по событию submit
 async function onSearchFormSubmit(event) {
   event.preventDefault();
+  //прячем кнопку
   loadMore.classList.add('is-hidden');
-
+  //Сеттером класса fetchSearch.query записываем значение инпута
   fetchSearch.query = event.currentTarget.elements.searchQuery.value;
-
+  //сбрасываем методом счетчик страниц
   fetchSearch.resetPage();
 
   try {
+    //в переменную response присваивавем результат значения метода fetchSearchImages() - это обьект
     const response = await fetchSearch.fetchSearchImages();
-    console.log(response);
-
+    console.log('~ response', response);
+    //если колличество картинок=0, то очищаем галерею, скрываем кнопку загрузить еще, сбрасываем поля формы
+    //и вывыодим сообщения с помощью библиотеки Notiflix
     if (response.data.hits.length === 0) {
       gallary.innerHTML = '';
       loadMore.classList.add('is-hidden');
@@ -39,28 +48,30 @@ async function onSearchFormSubmit(event) {
       );
       return;
     }
-
-    let totalHits = response.data.totalHits;
-    Notiflix.Notify.info(`Hooray! We found ${totalHits} images`);
-
+    //в переменную totalImages записываем колличество найденых картинок и выводим их в сообщении
+    let totalImages = response.data.totalHits;
+    Notiflix.Notify.info(`Hooray! We found ${totalImages} images`);
+    //если переменная класса totalHits=null, тогда ей присваеваем значение общего колличества картинок
     if (fetchSearch.totalHits === null) {
       fetchSearch.totalHits = response.data.totalHits;
     }
+    //отрисовываем наши картинки на странице с помощью библиотеки parcel-transformer-hbs
     gallary.innerHTML = templateCard(response.data.hits);
-
+    //вызываем библиотеку SimpleLightbox, а на ней метод refresh()
     libraryLithBox.refresh();
+    //отображаем кнопку
     loadMore.classList.remove('is-hidden');
   } catch (error) {
     console.log(error);
   }
 }
-
+//создаем функцию, которая рендерит наши картинки по кнопке загрузить еще
 function renderCards(data) {
   const card = templateCard(data);
   gallary.insertAdjacentHTML('beforeend', card);
   libraryLithBox.refresh();
 }
-
+//создаем функцию плавного скролла
 function smoothScroll() {
   const { height: cardHeight } = document
     .querySelector('.gallery')
@@ -72,19 +83,23 @@ function smoothScroll() {
   });
 }
 
+//Создаем асинхронную функцию по событию click
 async function onClickLoadMore(event) {
   try {
+    //если условие метода равно false, то выводим сообщениео том, что картинок для подзагрузки уже нет
     if (!fetchSearch.isNextDataExsist()) {
       Notiflix.Notify.info(
         "We're sorry, but you've reached the end of search results"
       );
-
+      //прячем кнопку
       loadMore.classList.add('is-hidden');
       return;
     }
 
     const response = await fetchSearch.fetchSearchImages();
+    //рендерим картинки
     renderCards(response.data.hits);
+    //запускаем плавный скролл
     smoothScroll();
   } catch (error) {
     console.log(error);
--- src/js/images-api.js
+++ src/js/images-api.js
@@ -1,15 +1,18 @@
-import axios from 'axios';
+import axios from 'axios'; //подключаем библиотеку для запросов на сервер
 
+//созаем и экспортируем класс по дефолту
 export default class FetchSearchImages {
+  //объявляем приватные переменные
   #BASE_URL = 'https://pixabay.com/api/';
   #API_KEY = '28704942-6968b84373f0d7bd37bb26e4e';
+  //создаем конструктор и обьявляем перемменные
   constructor() {
     this.searchQuery = '';
     this.page = 1;
     this.dataPerPage = 40;
     this.totalHits = null;
   }
-
+  //создаем метод (делаем его асинхронным), с помощью которого будем делать запросы на сервер
   async fetchSearchImages() {
     // const searchParam = new URLSearchParams({
     //   image_type: this.photo,
@@ -17,7 +20,7 @@ export default class FetchSearchImages {
     //   safesearch: this.true,
     //   per_page: this.dataPerPage,
     // });
-
+    //с помощью бибиотеки axios, делаем запрос, в котором 2-м аргументом прокидываем параметры запроса
     const request = await axios.get(`${this.#BASE_URL}`, {
       params: {
         image_type: this.photo,
@@ -29,20 +32,24 @@ export default class FetchSearchImages {
         key: this.#API_KEY,
       },
     });
-
+    //вызываем метод увелчения страниц на 1
     this.incrementPage();
     return request;
   }
+  //создаем метод увелчения страниц на 1
   incrementPage() {
     this.page += 1;
   }
+  //метод сброса страниц
   resetPage() {
     this.page = 1;
   }
+  //создаем метод для проверки, когда все картинки загрузились
+  //((тек.кол.страниц(4) - 1) * кол.стр.при выводе(40)) <= общ.кол.стр.
   isNextDataExsist() {
     return (this.page - 1) * this.dataPerPage <= this.totalHits;
   }
-
+  //геттер и сеттер нужного значения query
   get query() {
     return this.searchQuery;
   }
